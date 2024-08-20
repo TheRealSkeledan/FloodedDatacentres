b9a99473a17eb55a4c7922f4e7ed045aec2d7e3f
314PiGuy <147645262+314PiGuy@users.noreply.github.com>
2024-08-20 09:17:37 -0400

    Using animationplayer in animated sprite

--- FloodedDatacentres/characters/Guy/characterMovement.gd
+++ FloodedDatacentres/characters/Guy/characterMovement.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const SPEED = 60.0
 const JUMP_VELOCITY = -200.0
 
-@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var sprite: AnimationPlayer = $AnimatedSprite2D/AnimationPlayer
 @onready var handheldAnimation: AnimationPlayer = $handheldAnimation
 
 
@@ -19,9 +19,9 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("left", "right")
 	
 	if direction > 0:
-		sprite.flip_h = false
+		sprite.get_parent().flip_h = false
 	elif direction < 0:
-		sprite.flip_h = true
+		sprite.get_parent().flip_h = true
 
 	if direction == 0:
 		velocity.x = move_toward(velocity.x, 0, 15)
@@ -33,7 +33,7 @@ func _physics_process(delta: float) -> void:
 			sprite.play("walk", 2)
 			velocity.x = direction * SPEED * 2
 		else:
-			sprite.play("walk", 1)
+			sprite.play("walk", 0, 1)
 			handheldAnimation.play("walk")
 			velocity.x = direction * SPEED
 
