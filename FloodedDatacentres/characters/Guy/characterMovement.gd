f76bf16aa7ddb47dd4be4a6b4e0a56e9f9014961
314PiGuy <147645262+314PiGuy@users.noreply.github.com>
2024-08-20 10:12:53 -0400

    FIxed handheld animations

--- FloodedDatacentres/characters/Guy/characterMovement.gd
+++ FloodedDatacentres/characters/Guy/characterMovement.gd
@@ -4,8 +4,10 @@ extends CharacterBody2D
 const SPEED = 60.0
 const JUMP_VELOCITY = -200.0
 
-@onready var sprite: AnimationPlayer = $AnimatedSprite2D/AnimationPlayer
-@onready var handheldAnimation: AnimationPlayer = $handheldAnimation
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var spriteAnimation: AnimationPlayer = $AnimatedSprite2D/AnimationPlayer
+@onready var handheldAnimation: AnimationPlayer = $handheld/handheldAnimation
+@onready var handheld: Sprite2D = $handheld
 
 
 func _physics_process(delta: float) -> void:
@@ -18,23 +20,34 @@ func _physics_process(delta: float) -> void:
 
 	var direction := Input.get_axis("left", "right")
 	
-	if direction > 0:
-		sprite.get_parent().flip_h = false
-	elif direction < 0:
-		sprite.get_parent().flip_h = true
 
 	if direction == 0:
 		velocity.x = move_toward(velocity.x, 0, 15)
 		if abs(velocity.x) < 5:
-			sprite.play("idli")
-			handheldAnimation.play("idli")
-	else:
+			spriteAnimation.play("idli")
+			if sprite.flip_h:
+				handheldAnimation.play("idliLeft")
+			else:
+				handheldAnimation.play("idliRight")
+	elif direction > 0:
+		sprite.flip_h = false
 		if Input.is_action_pressed("shift"):
-			sprite.play("walk", 2)
+			spriteAnimation.play("walk", 0, 2)
+			handheldAnimation.play("right", 0, 2)
 			velocity.x = direction * SPEED * 2
 		else:
-			sprite.play("walk", 0, 1)
-			handheldAnimation.play("walk")
+			spriteAnimation.play("walk", 0, 1)
+			handheldAnimation.play("right", 0, 1)
+			velocity.x = direction * SPEED
+	else:
+		sprite.flip_h = true
+		if Input.is_action_pressed("shift"):
+			spriteAnimation.play("walk", 0, 2)
+			handheldAnimation.play("left", 0, 2)
+			velocity.x = direction * SPEED * 2
+		else:
+			spriteAnimation.play("walk", 0, 1)
+			handheldAnimation.play("left", 0, 1)
 			velocity.x = direction * SPEED
 
 	move_and_slide()
