4da411957884ea5029a71d734b8220d964b4ea82
314PiGuy <147645262+314PiGuy@users.noreply.github.com>
2024-08-23 16:27:43 -0400

    e

--- FloodedDatacentres/characters/Guy/guy.gd
+++ FloodedDatacentres/characters/Guy/guy.gd
@@ -11,7 +11,7 @@ func _ready() -> void:
 #
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("interact"):
+	if Input.is_action_just_pressed("interact"):
 		interacting = true
 	else:
 		interacting = false
---
+++ FloodedDatacentres/resources/person/Person.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+class_name ItemResource
+
+@export var name: String
+@export var health: float
+@export var speed: float
+@export var strength: float 
+@export var jump: float
+@exp
